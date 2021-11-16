04e2d847d0165d1979256f83c9754c3a831d75ae
John Jiang <zijiaj@student.ubc.ca>
2021-11-16 12:53:45 -0800

    collision detection rework

--- src/physics_system.cpp
+++ src/physics_system.cpp
@@ -2,36 +2,8 @@
 #include "physics_system.hpp"
 #include "world_init.hpp"
 
-// Returns the local bounding coordinates scaled by the current size of the entity
-vec2 get_bounding_box(const Motion &motion)
-{
-	if (motion.angle == 0.f)
-	{
-		return {abs(motion.scale.x), abs(motion.scale.y)};
-	}
-
-	Transform transform;
-	transform.rotate(motion.angle);
-	vec3 v1 = transform.mat * vec3{abs(motion.scale.x), abs(motion.scale.y), 1.f};
-	vec3 v2 = transform.mat * vec3{-abs(motion.scale.x), abs(motion.scale.y), 1.f};
-
-	return {max(abs(v1.x), abs(v2.x)), max(abs(v1.y), abs(v2.y))};
-}
-
-bool aabb_collides(const Motion &motion1, const Motion &motion2)
-{
-	const vec2 bb1 = get_bounding_box(motion1) / 2.f;
-	const vec2 bb2 = get_bounding_box(motion2) / 2.f;
-
-	return motion1.position.x - bb1.x < motion2.position.x + bb2.x &&
-		   motion1.position.x + bb1.x > motion2.position.x - bb2.x &&
-		   motion1.position.y - bb1.y < motion2.position.y + bb2.y &&
-		   motion1.position.y + bb1.y > motion2.position.y - bb2.y;
-}
-
 // get the world coordiante of collider vertices
-
-std::vector<vec2> get_world_coordinates(const Entity entity) {
+std::vector<vec2> get_vertices_location(Entity entity) {
 	Motion& motion = registry.motions.get(entity);
 
 	Transform transform;
@@ -39,100 +11,37 @@ std::vector<vec2> get_world_coordinates(const Entity entity) {
 	transform.rotate(motion.angle);
 	transform.scale(motion.scale);
 
-	Collider &collider = registry.colliders.get(entity);
+	std::vector<vec3> collider_vertices = registry.colliders.get(entity).vertices;
 	std::vector<vec2> vertices;
-	for (vec3 &vertex : collider.vertices)
+	for (vec3& vertex : collider_vertices)
 	{
 		vec3 transformed_vertex = transform.mat * vertex;
 		vertices.push_back(vec2(transformed_vertex.x, transformed_vertex.y));
 	}
-	//printf("size world: %d\n", vertices.size());
 	return vertices;
 }
 
-// convex polygons collision detection
-// entity_1 can be a wall entity or a non wall entity
-// entity_2 cannot be a wall entity
-bool convex_polygons_collides(const Entity entity_1, const Entity entity_2) {
+// Returns x min y min x max y max in vec4
+std::vector<float> get_bounding_box(Entity entity)
+{
+	std::vector<vec2> vertices = get_vertices_location(entity);
+	std::vector<float> bb = { vertices[0].x, vertices[0].y, vertices[0].x, vertices[0].y };
 
-	bool resolve_collision = false;
-	const Entity *e1 = &entity_1;
-	const Entity *e2 = &entity_2;
-
-	// e2 should be wall
-	if (registry.walls.has(entity_1))
-	{
-		resolve_collision = true;
-		e1 = &entity_2;
-		e2 = &entity_1;
-	}
-	else if (registry.walls.has(entity_2))
-	{
-		resolve_collision = true;
+	for (int i = 1; i < vertices.size(); i++) {
+		bb[0] = min(bb[0], vertices[i].x);
+		bb[1] = min(bb[1], vertices[i].y);
+		bb[2] = max(bb[2], vertices[i].x);
+		bb[3] = max(bb[3], vertices[i].y);
 	}
 
-	// get vertex coordinates in world frame
-	std::vector<vec2> vertices_1 = get_world_coordinates(*e1);
-	std::vector<vec2> vertices_2 = get_world_coordinates(*e2);
+	/*printf("x_min: %f y_min: %f x_max: %f x_max: %f\n", bb[0], bb[0], bb[2], bb[3]);*/
 
-	std::vector<vec2> v1 = vertices_1;
-	std::vector<vec2> v2 = vertices_2;
+	return bb;
+}
 
-	assert(!(registry.walls.has(*e1) && registry.walls.has(*e2)));
-
-	Motion &e1_motion = registry.motions.get(*e1);
-	vec2 line_1_vertex_1 = e1_motion.position;
-
-	for (int i = 0; i < 2; i++)
-	{
-		for (vec2 line_1_vertex_2 : v1)
-		{
-			vec2 dir_1 = line_1_vertex_2 - line_1_vertex_1;
-			for (int j = 0; j < v2.size(); j++)
-			{
-				vec2 line_2_vertex_1 = v2[j];
-				vec2 dir_2 = v2[(j + 1) % v2.size()] - line_2_vertex_1;
-
-				vec2 k = line_1_vertex_1 - line_2_vertex_1;
-				float det = dir_1.x * dir_2.y - dir_2.x * dir_1.y;
-
-				if (det == 0.f)
-				{
-					continue;
-				}
-
-				float t1 = (dir_2.x * k.y - dir_2.y * k.x) / det;
-				float t2 = (dir_1.x * k.y - dir_1.y * k.x) / det;
-
-				// if intersects
-				if (t1 > 0.f && t1 < 1.f && t2 > 0.f && t2 < 1.f)
-				{
-					//printf("%f   %f\n", t1, t2);
-					if (resolve_collision)
-					{
-						vec2 normalized_normal = normalize(vec2{-dir_2.y, dir_2.x});
-						vec2 displacement = dot((1.f - t1) * dir_1, normalized_normal) * normalized_normal;
-						if (i == 1)
-						{
-							displacement = -displacement;
-						}
-						e1_motion.position -= displacement;
-						for (vec2 &v : v1)
-						{
-							v -= displacement;
-						}
-						continue;
-					}
-					return true;
-				}
-			}
-		}
-		v1 = vertices_2;
-		v2 = vertices_1;
-		line_1_vertex_1 = registry.motions.get(*e2).position;
-	}
-
-	return false;
+bool aabb_collides(std::vector<float> bb1, std::vector<float> bb2)
+{
+	return bb1[0] < bb2[2] && bb1[2] > bb2[0] && bb1[1] < bb2[3] && bb1[3] > bb2[1];
 }
 
 bool point_convex_polygon_collides(const Entity entity_1, const Entity entity_2) {
@@ -147,7 +56,7 @@ bool point_convex_polygon_collides(const Entity entity_1, const Entity entity_2)
 		e2 = &entity_1;
 	}
 
-	std::vector<vec2> v2 = get_world_coordinates(*e2);
+	std::vector<vec2> v2 = get_vertices_location(*e2);
 
 	vec2 line_1_vertex_1 = registry.motions.get(*e1).position;
 	vec2 line_1_vertex_2 = registry.motions.get(*e2).position;
@@ -178,24 +87,6 @@ bool point_convex_polygon_collides(const Entity entity_1, const Entity entity_2)
 	return true;
 }
 
-// This is a SUPER APPROXIMATE check that puts a circle around the bounding boxes and sees
-// if the center point of either object is inside the other's bounding-box-circle. You can
-// surely implement a more accurate detection
-bool collides(const Entity entity_1, const Entity entity_2)
-{
-	if (aabb_collides(registry.motions.get(entity_1), registry.motions.get(entity_2)))
-	{
-		/*if (registry.players.has(entity_1) || registry.players.has(entity_2)) {
-			printf("Collides: %f\n", registry.motions.get(entity_1).position.x);
-		}*/
-		if (registry.bullets.has(entity_1) || registry.bullets.has(entity_2)) {
-			return point_convex_polygon_collides(entity_1, entity_2);
-		}
-		return convex_polygons_collides(entity_1, entity_2);
-	}
-	return false;
-}
-
 void PhysicsSystem::step(float elapsed_ms)
 {
 	// Move fish based on how much time has passed, this is to (partially) avoid
@@ -204,7 +95,6 @@ void PhysicsSystem::step(float elapsed_ms)
 	auto &motion_registry = registry.motions;
 	for (uint i = 0; i < motion_registry.size(); i++)
 	{
-		// !!! TODO A1: update motion.position based on step_seconds and motion.velocity
 		Motion &motion = motion_registry.components[i];
 		Entity entity = motion_registry.entities[i];
 		float step_seconds = 1.0f * (elapsed_ms / 1000.f);
@@ -220,71 +110,91 @@ void PhysicsSystem::step(float elapsed_ms)
 		}
 	}
 
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	// TODO A3: HANDLE PEBBLE UPDATES HERE
-	// DON'T WORRY ABOUT THIS UNTIL ASSIGNMENT 3
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	//ComponentContainer<Collider> &collider_container = registry.colliders;
+	//for (uint i = 0; i < collider_container.size(); i++)
+	//{
+	//	Entity entity_i = collider_container.entities[i];
+	//	for (uint j = i + 1; j < collider_container.size(); j++) // i+1
+	//	{
+	//		assert(i != j);
 
-	// Check for collisions between all moving entities
-	ComponentContainer<Collider> &collider_container = registry.colliders;
-	for (uint i = 0; i < collider_container.size(); i++)
-	{
-		Entity entity_i = collider_container.entities[i];
-		for (uint j = i + 1; j < collider_container.size(); j++) // i+1
-		{
-			assert(i != j);
-
-			Entity entity_j = collider_container.entities[j];
+	//		Entity entity_j = collider_container.entities[j];
 
 
-			// walls shouldn't be colliding
-			if (registry.walls.has(entity_i) && registry.walls.has(entity_j)) {
-				continue;
-			}
+	//		// walls shouldn't be colliding
+	//		if (registry.walls.has(entity_i) && registry.walls.has(entity_j)) {
+	//			continue;
+	//		}
 
-			if (registry.bullets.has(entity_i) && registry.bullets.has(entity_j)) {
-				continue;
-			}
+	//		if (registry.bullets.has(entity_i) && registry.bullets.has(entity_j)) {
+	//			continue;
+	//		}
 
-			if (collides(entity_i, entity_j))
-			{
+	//		if (collides(entity_i, entity_j))
+	//		{
 
-				for (auto callback : callbacks) {
+	//			for (auto callback : callbacks) {
+	//				callback(entity_i, entity_j);
+	//			}
+	//		}
+	//	}
+	//}
 
-					callback(entity_i, entity_j);
-				}
+	// all walls' bounding box
+	// x min y min x max y max
+	std::vector<std::vector<float>> wall_bb;
+	for (auto w : registry.walls.entities) {
+		wall_bb.push_back(get_bounding_box(w));
+		/*float* bb = get_bounding_box(w);
+		printf("x_min: %f y_min: %f x_max: %f x_max: %f\n", bb[0], bb[0], bb[2], bb[3]);*/
+	}
+
+	// all bullets' bounding box
+	// in reversed order
+	std::vector<std::vector<float>> bullet_bb;
+	for (Entity e : registry.bullets.entities) {
+		bullet_bb.push_back(get_bounding_box(e));
+	}
+
+	// bullets vs walls
+	// in reversed order
+	for (int i = registry.bullets.entities.size() - 1; i >= 0; i--) {
+		for (std::vector<float> bb : wall_bb) {
+			//printf("bullet   x_min: %f y_min: %f x_max: %f x_max: %f\n", bullet_bb[i][0], bullet_bb[i][1], bullet_bb[i][2], bullet_bb[i][3]);
+			//printf("wall   x_min: %f y_min: %f x_max: %f x_max: %f\n", bb[0], bb[1], bb[2], bb[3]);
+			if (aabb_collides(bullet_bb[i], bb)) {
+				registry.remove_all_components_of(registry.bullets.entities[i]);
+				bullet_bb[i] = bullet_bb.back();
+				bullet_bb.pop_back();
+				break;
 			}
 		}
 	}
 
-
-	ComponentContainer<Motion>& motion_container = registry.motions;
-
 	// debugging of bounding boxes
 	if (debugging.in_debug_mode)
 	{
-		uint size_before_adding_new = (uint)motion_container.components.size();
+		uint size_before_adding_new = (uint)registry.motions.entities.size();
 		for (uint i = 0; i < size_before_adding_new; i++)
 		{
-			Motion &motion_i = motion_container.components[i];
-			Entity entity_i = motion_container.entities[i];
-
+			Motion &motion = registry.motions.components[i];
+			Entity entity = registry.motions.entities[i];
 
 			// visualize axises
-			Entity line1 = createLine(motion_i.position, motion_i.angle, vec2{ 30.f, 3.f});
-			Entity line2 = createLine(motion_i.position, motion_i.angle, vec2{ 3.f, 30.f });
+			Entity line1 = createLine(motion.position, motion.angle, vec2{ 30.f, 3.f});
+			Entity line2 = createLine(motion.position, motion.angle, vec2{ 3.f, 30.f });
 
 
-			if (registry.colliders.has(entity_i)) {
-				std::vector<vec2> transformed_verticies = get_world_coordinates(entity_i);
+			if (registry.colliders.has(entity)) {
+				std::vector<vec2> vertices = get_vertices_location(entity);
 
-				for (int i = 0; i < transformed_verticies.size(); i++) {
-					vec2 vector1 = transformed_verticies[i];
-					vec2 vector2 = transformed_verticies[(i + 1) % transformed_verticies.size()];
-					vec2 dir = vector2 - vector1;
-					float angle = atan2(dir.y, dir.x);
-					vec2 pos = (vector2 + vector1) / 2.f;
-					createLine(pos, angle, vec2{ glm::length(dir), 3.f });
+				for (int i = 0; i < vertices.size(); i++) {
+					vec2 v1 = vertices[i];
+					vec2 v2 = vertices[(i + 1) % vertices.size()];
+					vec2 edge = v2 - v1;
+					float angle = atan2(edge.y, edge.x);
+					vec2 pos = (v2 + v1) / 2.f;
+					createLine(pos, angle, vec2{ glm::length(edge), 3.f });
 				}
 			}
 		}
